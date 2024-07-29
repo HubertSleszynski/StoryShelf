0ba4d25f4518ba431aab127007531f4dabe96d13
Hubert Śleszyński <h.r.sleszynski@gmail.com>
2024-07-29 12:04:13 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongodbURL =
-  "mongodb+srv://hsleszynski:Aa1234$@clusterbookshelf.1xxqsle.mongodb.net/book-collection?retryWrites=true&w=majority&appName=ClusterBookShelf";
+export const mongodbURL =""
